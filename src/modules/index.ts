92062d4bddeca3018e5666f80b47b5f588df274f
Daniel Brad <daniel.brad@concur.com>
2019-09-13 22:37:27 -0700

    reset state on logout

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -7,6 +7,7 @@ import {LoginState} from "modules/login/loginTypes";
 import {UserState} from "modules/user/userTypes";
 import {BasketsState} from "modules/baskets/basketsTypes";
 import {InitializationState} from "modules/initialization/initializationTypes";
+import {LogoutActionConstants} from "./logout/logoutTypes";
 
 export type State = {
     user: UserState;
@@ -22,6 +23,16 @@ const reducer = combineReducers({
     baskets
 });
 
-export default reducer;
+// reset all state nodes other than initialization on logout
+function rootReducer(state: any, action: any) {
+    if (action.type === LogoutActionConstants.DID_LOGOUT) {
+        state = {initialization: {initialized: true}};
+    }
+
+    return reducer(state, action);
+};
+
+export default rootReducer;
+
 
 export type AppState = ReturnType<typeof reducer>;
